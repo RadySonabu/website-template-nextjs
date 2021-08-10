24ebaa1c10f70ef4def10bd080895b9330bc072a
RadySonabu <ardynubanos@yahoo.com>
2021-08-10 22:07:02 +0800

    Added Orders compnent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "@fontsource/roboto";
+import Layout from "../components/Layout";
+import React, { FC } from "react";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
+	);
 }
-export default MyApp
+export default MyApp;
